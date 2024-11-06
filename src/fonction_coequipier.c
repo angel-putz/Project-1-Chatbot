d1e4c4237cfa5d9cede1b362f737ec7a3ac43e8a
angel-putz <angel.putz@laplateforme.io>
2024-11-06 13:19:42 +0100

    maj

--- src/fonction_coequipier.c
+++ src/fonction_coequipier.c
@@ -1,5 +1,25 @@
 
 void coequipier (void)
 {
+    if (demande == "manger")
+    {
+        if (type == "cuistot present")
+        {
 
+        } else if (type == "cuistot absent")
+        {
+
+        }
+
+    }
+    else if (demande == "soins")
+    {
+        if (type == "soigneur absent")
+        {
+
+        } else if (type == "soigneur present")
+        {
+
+        }
+    }
 }
